cc1bd2b5ea3bc1db12d852dbbd10e54558ce19ac
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-09-24 03:44:13 -0300

    chore: Adicionadas dependências class-transformer e class-validator feat: Implementar pipes de validação e DTOs para criação e atualização de tarefas

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove valores que não estão nos DTOs
+      forbidNonWhitelisted: true, // Retorna erro se um valor não estiver no DTO
+      transform: true, // Transforma os dados para os tipos especificados nos DTOs
+    }),
+  );
+
+
   await app.listen(3000);
 }
 bootstrap();
--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,15 @@
-export class CreateTaskDto {}
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateTaskDto {
+    @IsString()
+    @IsNotEmpty({ message: 'O título não pode estar vazio.' })
+    title: string;
+
+    @IsString()
+    @IsNotEmpty({ message: 'A descrição não pode estar vazia.' })
+    description: string;
+
+    @IsString()
+    @IsOptional()
+    status?: string;
+}
