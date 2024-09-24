8f3782d9231b9371a05c1efd5c3eb5ee4a8d4273
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-09-24 03:28:44 -0300

    feat: criar as entidades DefaultEntity, User e Task

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,1 +1,19 @@
-export class Task {}
+import { DefaultEntity } from "shared/database/entities/default.entity";
+import { User } from "src/user/entities/user.entity";
+import { Column, ManyToOne, OneToMany } from "typeorm";
+
+export class Task extends DefaultEntity<Task> {
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: string;
+
+
+    @ManyToOne(() => User, (user) => user.tasks)
+    user: User;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { DefaultEntity } from "shared/database/entities/default.entity";
+import { Task } from "src/task/entities/task.entity";
+import { Column, OneToMany } from "typeorm";
+
+export class User extends DefaultEntity<User> {
+
+    constructor(data?: Partial<User>) {
+        super(data);
+    }
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    name: string;
+
+
+    @OneToMany(() => Task, (task) => task.user)
+    tasks: Task[];
+
+}
