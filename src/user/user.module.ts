3d863c2fc067800dc9b50c3e50cd3b490bbc8f1a
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-09 03:02:38 -0300

    refactor: ajustar módulo de usuário para utilizar UserRepository com factory
    Refatora o UserModule para utilizar o UserRepository através de uma factory que injeta o DataSource do TypeORM. Remove a importação do TypeOrmModule e ajusta os provedores para garantir que o UserRepository seja criado corretamente e disponível para o UserService.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { User } from './entities/user.entity';
+import { DataSource } from 'typeorm';
 
 @Module({
+  imports: [],
+  providers: [
+    UserService,
+    {
+      provide: UserRepository,
+      useFactory: (dataSource: DataSource) => new UserRepository(dataSource),
+      inject: [DataSource],
+    },
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
