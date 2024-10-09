f434f72ac40d4dcf702bb680fc81206e5d41f383
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-09 03:08:24 -0300

    refactor: ajustar AuthModule para incluir estratégias de autenticação e serviços necessários
    Refatora o AuthModule para incluir as estratégias JwtStrategy e LocalStrategy, além do AESEncryptionService como provedores. Importa o UserModule para garantir acesso aos serviços de usuário. Adiciona o JwtModule com configuração padrão para geração de tokens JWT.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { AESEncryptionService } from 'src/shared/config/encryption/aes.encryption.service';
+import { UserRepository } from 'src/user/user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from 'src/user/user.service';
+import { UserModule } from 'src/user/user.module';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secretKey',
+      signOptions: { expiresIn: '60m' },
+    }),
+    UserModule 
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, LocalStrategy, AESEncryptionService],
 })
 export class AuthModule {}
