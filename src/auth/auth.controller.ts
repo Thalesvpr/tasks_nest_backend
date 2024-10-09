4e961f1ceed678c7f407dff64a8f363e865ff4d5
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-09 03:08:58 -0300

    feat: implementar rotas de registro e login no AuthController
    Adiciona rotas de `register` e `login` no AuthController. Utiliza `LocalGuard` para autenticação local na rota de login e gera tokens JWT utilizando o AuthService. Implementa a rota de registro para criar novos usuários e delega ao AuthService o tratamento de lógica de negócios.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,34 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, HttpStatus, UseFilters } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginPayloadDto } from './dto/login-payload.dto';
+import { RegisterPayloadDto } from './dto/register-payload.dto';
+import { LocalGuard } from './guard/local.guard';
+import { NewEntity } from 'src/shared/entities/default.entity';
+import { User } from 'src/user/entities/user.entity';
+import { SystemExceptionFilter } from 'src/shared/filters/exception.filter';
+import { CurrentUser } from 'src/shared/decorators/user.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  @Post('login')
+  @UseGuards(LocalGuard)
+  login(@CurrentUser() user: User) {
+    const result = this.authService.generateJwt(user);
+    return result
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('register')
+  register(@Body() registerPayloadDto: RegisterPayloadDto, @Req() req: Request) {
+    const newUser = new User(registerPayloadDto)
+    const result = this.authService.register(newUser);
+    return result
   }
 }
