1a4451d6032de01943d49b3cfc1c910067daa686
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-09 03:07:11 -0300

    refactor: remover filtros globais de exceção obsoletos do bootstrap
    Remove os filtros globais ValidationExceptionFilter e HttpExceptionFilter do bootstrap em main.ts, simplificando a inicialização da aplicação. Os filtros foram removidos por não serem mais necessários devido às recentes mudanças na arquitetura do projeto.

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
-import { ValidationExceptionFilter } from './shared/filters/validation-excepion';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,11 +13,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // Retorna erro se um valor não estiver no DTO
       transform: true, // Transforma os dados para os tipos especificados nos DTOs
     }),
-  );
-
-  app.useGlobalFilters(
-    new ValidationExceptionFilter(configService),
-    new HttpExceptionFilter(configService),
   );
 
   await app.listen(3000);
