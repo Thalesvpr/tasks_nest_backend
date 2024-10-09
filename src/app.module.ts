be74c43099822461ae776a5b4dbc0d38c16be65c
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-09 03:10:10 -0300

    feat: adicionar filtro global de exceção e inicialização de configuração no AppModule
    Atualiza o AppModule para implementar OnModuleInit, utilizando o AppConfig para inicializar as configurações globais. Adiciona o SystemExceptionFilter como filtro global para tratamento de exceções, garantindo um gerenciamento consistente de erros em toda a aplicação.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './shared/database/database.module';
+import { AppConfig } from './shared/config/app.config';
+import { SystemExceptionFilter } from './shared/filters/exception.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -16,6 +19,18 @@ import { DatabaseModule } from './shared/database/database.module';
     ),
     DatabaseModule, TaskModule, UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  
+  providers: [AppService,
+    {
+      provide: APP_FILTER,
+      useClass: SystemExceptionFilter,
+  },
+  ],
 })
-export class AppModule { }
+export class AppModule implements OnModuleInit {
+  constructor(private configService: ConfigService) {}
+
+  onModuleInit() {
+    AppConfig.initialize(this.configService);
+  }
+}
