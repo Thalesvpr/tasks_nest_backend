5c1a5b04b5c1b655a1e8ef430ffc9afc79dd68fc
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-09 03:02:13 -0300

    refactor: refatorar UserController para integração com UserService e uso de CurrentUser
    Refatora o UserController para utilizar o UserService de maneira assíncrona, adicionando métodos mais descritivos como getAllUsers, getUserById, createUser, updateUser e deleteUser. Inclui o uso do decorator CurrentUser para obter o usuário atual em operações de criação, atualização e exclusão. Altera as anotações do controlador para integrar com AccessGuard e melhorar a segurança.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,34 +1,52 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, HttpStatus, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { AccessGuard } from 'src/auth/guard/access.guard';
+import { CurrentUser } from 'src/shared/decorators/user.decorator';
 
-@Controller('user')
+@UseGuards(AccessGuard)
+@Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
+  constructor(private readonly userService: UserService) { }
 
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  async getAllUsers(): Promise<User[]> {
+
+    const result = await this.userService.findAll();
+    return result;
+
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  async getUserById(@Param('id') id: string): Promise<User | null> {
+
+    const result = await this.userService.findOne(id);
+    return result
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto, @CurrentUser() currentUser: User): Promise<User> {
+
+    const newUser = new User(createUserDto);
+
+    const result = await this.userService.create(newUser);
+    return result
+
+  }
+
+  @Put(':id')
+  async updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto, @CurrentUser() currentUser: User): Promise<User> {
+    const result = await this.userService.update(id, updateUserDto);
+    return result
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  async deleteUser(@Param('id') id: string, @CurrentUser() currentUser: User): Promise<void> {
+
+    await this.userService.remove(id);
+
+
   }
 }
