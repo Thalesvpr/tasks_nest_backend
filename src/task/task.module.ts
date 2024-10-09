025276e5c3df494071e2bffcc6704ddadee936d0
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-09 03:04:12 -0300

    refactor: ajustar TaskModule para remover TypeOrmModule e TaskRepository dos provedores
    Refatora o TaskModule removendo a importação do TypeOrmModule e o TaskRepository dos provedores, mantendo apenas o TaskService como provedor. Simplifica a configuração do módulo para refletir as mudanças realizadas no TaskService e TaskRepository.

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { TaskRepository } from './task.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TaskRepository])],
   controllers: [TaskController],
-  providers: [TaskService, TaskRepository],
+  providers: [TaskService],
 })
 export class TaskModule {}
