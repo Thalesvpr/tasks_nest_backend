ff577d6b83030c24df2b5f6413465a808872f19e
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-09 03:04:51 -0300

    refactor: simplificar TaskController removendo ControllerResult e adicionando controle de acesso ao usuário atual
    Refatora o TaskController para remover o uso de ControllerResult, retornando diretamente os resultados dos métodos do TaskService. Adiciona o decorator CurrentUser para obter o usuário atual e garantir que apenas o criador possa atualizar ou deletar suas próprias tarefas. Ajusta os métodos CRUD para refletir as mudanças no TaskService.

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,49 +1,51 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, HttpStatus, UseGuards } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { Task } from './entities/task.entity';
-import { ControllerResult } from 'src/shared/results/controller.result';
+import { AccessGuard } from 'src/auth/guard/access.guard';
+import { CurrentUser } from 'src/shared/decorators/user.decorator';
+import { User } from 'src/user/entities/user.entity';
 
+@UseGuards(AccessGuard)
 @Controller('tasks')
 export class TaskController {
-  constructor(private readonly taskService: TaskService) {}
+  constructor(private readonly taskService: TaskService) { }
 
   @Get()
-  async getAllTasks(): Promise<ControllerResult<Task[]>> {
+  async getAllTasks(): Promise<Task[]> {
     const allTasks = await this.taskService.getAllTasks();
-    return new ControllerResult<Task[]>(allTasks.data, HttpStatus.OK);
+    return allTasks
   }
 
   @Get(':id')
-  async getTaskById(@Param('id') id: string): Promise<ControllerResult<Task | null>> {
+  async getTaskById(@Param('id') id: string): Promise<Task | null> {
     const task = await this.taskService.getTaskById(id);
-    return new ControllerResult<Task | null>(task.data, HttpStatus.OK);
+    return task
   }
 
   @Post()
-  async createTask(@Body() createTaskDto: CreateTaskDto): Promise<ControllerResult<Task>> {
+  async createTask(@Body() createTaskDto: CreateTaskDto, @CurrentUser() user: User): Promise<Task> {
     const task = new Task(createTaskDto);
-    const newTask = await this.taskService.createTask(task);
-    return new ControllerResult<Task>(newTask.data, HttpStatus.CREATED);
+    const newTask = await this.taskService.createTask(task, user.id);
+    return newTask
   }
 
   @Put(':id')
-  async updateTask(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto): Promise<ControllerResult<Task>> {
-    const updatedTask = await this.taskService.updateTask(id, updateTaskDto);
-    
-    return new ControllerResult<Task>(updatedTask.data, HttpStatus.OK);
+  async updateTask(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto, @CurrentUser() user: User): Promise<Task> {
+    const updatedTask = await this.taskService.updateTask(id, updateTaskDto, user.id);
+    return updatedTask
   }
 
   @Delete(':id')
-  async deleteTask(@Param('id') id: string): Promise<ControllerResult<void>> {
-    await this.taskService.deleteTask(id);
-    return new ControllerResult<void>(null, HttpStatus.NO_CONTENT);
+  async deleteTask(@Param('id') id: string, @CurrentUser() user: User): Promise<void> {
+    const deleted = await this.taskService.deleteTask(id, user.id);
   }
 
   @Get('user/:userId')
-  async getTasksByUserId(@Param('userId') userId: string): Promise<ControllerResult<Task[]>> {
+  async getTasksByUserId(@Param('userId') userId: string): Promise<Task[]> {
     const userTasks = await this.taskService.getTasksByUserId(userId);
-    return new ControllerResult<Task[]>(userTasks.data, HttpStatus.OK);
+    return userTasks
   }
 }
+
