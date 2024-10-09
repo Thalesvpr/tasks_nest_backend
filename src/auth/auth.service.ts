6d86087229c43936e90c98772ec66f7a4d60e8da
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-09 03:07:58 -0300

    feat: implementar registro de usuário e verificação de senha no AuthService
    Refatora o AuthService para incluir métodos de registro de usuário (`register`) e verificação de senha (`verifyUser` e `verifyPassword`). Utiliza UserService para verificar a existência do usuário e AESEncryptionService para comparar senhas. Também adiciona o método `generateJwt` para gerar tokens JWT após autenticação bem-sucedida.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,67 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { NewEntity } from 'src/shared/entities/default.entity';
+import { UserRepository } from 'src/user/user.repository';
+import { User } from 'src/user/entities/user.entity';
+import { ConflictAuthError, BadRequestAuthError, NotFoundAuthError } from 'src/shared/exceptions/auth.exceptions';
+import { AESEncryptionService } from 'src/shared/config/encryption/aes.encryption.service';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private readonly userService: UserService,
+    private readonly encryptionService: AESEncryptionService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async register(data: NewEntity<User>): Promise<User> {
+    const existingUser = await this.userService.userExist(data.email);
+    if (existingUser) {
+      throw new ConflictAuthError();
+    }
+    const newUser = new User(data);
+    const createdUser = await this.userService.create(newUser);
+    if (!createdUser) {
+      throw new BadRequestAuthError();
+    }
+
+    return createdUser;
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async generateJwt(user: User) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  /**
+   * Verifica se o usuário existe e se a senha está correta.
+   */
+  async verifyUser(email: string, password: string): Promise<User | null> {
+    const user = await this.userService.findByEmail(email);
+
+    if (!user) {
+      return null; 
+    }
+
+    const passwordValid = await this.verifyPassword(password, user.password);
+    console.log(passwordValid)
+    if (!passwordValid) {
+      return null;  
+    }
+
+    return user;
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
+  /**
+   * Compara a senha fornecida com a senha armazenada no banco de dados.
+   */
+  private async verifyPassword(password: string, hashedPassword: string): Promise<boolean> {
+    return this.encryptionService.compare(password, hashedPassword);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+
+  
 }
